6c310dccadb405e33b6a3df43be1c6a51b38f124
Caleb Kelly <calebaid2000@gmail.com>
2024-07-30 23:18:08 +0800

    original stokes function

--- src/kernels.py
+++ src/kernels.py
@@ -5,31 +5,55 @@
 ############################################################
 
 from numpy import (
-    cos, sin, radians,
-    sqrt, log
+    sin, cos, radians, 
+    sqrt, log, array
 )
 
-def stokes(lonp, latp, lon, lat):
+def stokes(comp_point, int_points):
     '''
-    Original Stokes' kernel 
+    Calculate the Original Stokes' function
     
     Parameters
     ----------
-    lonp      : longitude of computation point (in degrees)
-    latp      : latitude of computation point (in degrees)
-    lon       : longitude of running/integrating points (in degrees)
-    lat       : latitude of running/integrating points (in degrees)
+    comp_point : array-like, shape (2,)
+                    [lon, lat] of computation point
+    int_points : array-like, shape (n, 2)
+                    [lon, lat] of integration points
     
     Returns
     -------
-    S         : Stokes' kernel
-    sph_dist  : Spherical distance
-    '''
+    S         : Stokes' function
+    cos_psi   : Cosine of spherical distance
     
-    lonp = radians(lonp)
-    latp = radians(latp)
-    lon  = radians(lon)
-    lat  = radians(lat)
+    Notes
+    -----
+    1. https://en.wikipedia.org/wiki/Haversine_formula
+    2. cos(a - b) = cos(a)cos(b) + sin(a)sin(b)
+    3. Spherical cosines
+    4. Estimate sin2_psi_2 = sin^2(psi/2) using Haversine formula (Note 1)
+    5. Physical Geodesy (2nd Edition) Page 104, Equation 2–305
+    '''
+    lonp, latp = array(comp_point)
+    int_points = array(int_points) if isinstance(int_points, list) else int_points
+    lon, lat = int_points[:, 0], int_points[:, 1]
+    
+    lon, lat, lonp, latp = radians(lon), radians(lat), radians(lonp), radians(latp)
+    
+    # Calculate cos_dlam using spherical trigonometry (Note 2)
+    cos_dlam = cos(lon) * cos(lonp) + sin(lon) * sin(lonp)  
+    
+    # Calculate cos_psi using the spherical law of cosines (Note 3)
+    cos_psi = sin(latp) * sin(lat) + cos(latp) * cos(lat) * cos_dlam  
+    
+    # Calculate sin^2(psi/2) using the Haversine formula (Note 4)
+    sin2_psi_2 = sin( (latp - lat)/2 )**2 + cos(latp) * cos(lat) * sin( (lonp - lon)/2 )**2 
+    
+    # Calculate Stokes' function S (Note 5)
+    S = 1/sqrt(sin2_psi_2) - 6*sqrt(sin2_psi_2) + 1 - 5*cos_psi - \
+        3*cos_psi*log(sqrt(sin2_psi_2) + sin2_psi_2)
+        
+    return S, cos_psi
+    
     
 def stokes_func(sph_dist):
     '''
@@ -47,62 +71,11 @@ def stokes_func(sph_dist):
     ---------
     1. Physical Geodesy (2nd Edition) Page 104, Equation 2–305
     2. For numerical efficiency and accuracy, we will use a slightly modified form of Equation 2-305
-    '''
-    # S = 1 / sqrt(sph_dist) - 6 * sqrt(sph_dist) + 1 - 5 * cos(sph_dist) - \
-    #     3 * cos(sph_dist) * log(sqrt(sph_dist) + sph_dist)
-    
+    '''    
     S = 1/sin(sph_dist/2) - 6*sin(sph_dist/2) + 1 - 5*cos(sph_dist) - \
         3*cos(sph_dist)*log(sin(sph_dist/2) + sin(sph_dist/2)**2)
     
     return S
-
-# def S_Stokes(lon, lat, lonp, latp):
-#     """
-#     Compute Stokes' function and the cosine of the spherical distance between points.
-    
-#     Parameters:
-#     lon (array): Longitudes of integrating points.
-#     lat (array): Latitudes of integrating points.
-#     lonp (float): Longitude of the computation point.
-#     latp (float): Latitude of the computation point.
-    
-#     Returns:
-#     S (array): Stokes' function values.
-#     cos_psi (array): Cosine of the spherical distance between points.
-#     """
-#     # Convert degrees to radians if necessary
-#     lon = np.radians(lon)
-#     lat = np.radians(lat)
-#     lonp = np.radians(lonp)
-#     latp = np.radians(latp)
-
-#     # Cosine of the longitude difference
-#     cos_dlam = np.cos(lon) * np.cos(lonp) + np.sin(lon) * np.sin(lonp)
-
-#     # Cosine of the spherical distance (cosine of the central angle)
-#     cos_psi = np.sin(latp) * np.sin(lat) + np.cos(latp) * np.cos(lat) * cos_dlam
-
-#     # Auxiliary quantity (square of the sine of half the spherical distance)
-#     sin2_psi_2 = np.sin((latp - lat) / 2) ** 2 + \
-#                  np.sin((lonp - lon) / 2) ** 2 * \
-#                  np.cos(latp) * np.cos(lat)
-
-#     # Stokes's function (using the auxiliary quantity and the cosine of the spherical distance)
-#     S = 1 / np.sqrt(sin2_psi_2) - 6 * np.sqrt(sin2_psi_2) + 1 - 5 * cos_psi - \
-#         3 * cos_psi * np.log(np.sqrt(sin2_psi_2) + sin2_psi_2)
-
-#     return S, cos_psi
-
-# Example usage
-# lon = np.array([0, 1, 2])
-# lat = np.array([0, 1, 2])
-# lonp = 0.5
-# latp = 0.5
-
-# S, cos_psi = S_Stokes(lon, lat, lonp, latp)
-# print("Stokes' function values:", S)
-# print("Cosine of spherical distances:", cos_psi)
-    
     
 
 def wong_and_gore():
