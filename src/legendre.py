d9ff527c00c616d591a7812b63157808bbab7523
Caleb Kelly <calebaid2000@gmail.com>
2024-08-04 19:30:42 +0800

    update `legendre.ALF()`

--- src/legendre.py
+++ src/legendre.py
@@ -6,17 +6,19 @@
 import constants
 from numpy import (
     sin, cos, radians, 
-    zeros, sqrt
+    zeros, sqrt, degrees
 )
-from coordinates import geodetic2geocentric
+from coordinates import geodetic2spherical
  
-def ALF(phi, nmax=60, ellipsoid='wgs84'):
+def ALF(phi=None, lambd=None, vartheta=None, nmax=60, ellipsoid='wgs84'):
     '''
     Compute associated Legendre functions
 
     Parameters
     ----------
     phi       : geodetic latitude (degrees)
+    vartheta  : colatitude (degrees)
+    lambd     : geodetic longitude (degrees)
     nmax      : maximum degree of expansion
     ellipsoid : reference ellipsoid ('wgs84' or 'grs80')
     
@@ -30,17 +32,19 @@ def ALF(phi, nmax=60, ellipsoid='wgs84'):
     summation and the recursive computation of very high degree and order 
     normalised associated Legendre functions (Eqs. 11 and 12)
     '''
-
-    # Select ellipsoid parameters
-    ref_ellipsoid = constants.wgs84() if 'wgs84' in ellipsoid.lower() else constants.grs80()
-    semi_major = ref_ellipsoid['semi_major']
-    semi_minor = ref_ellipsoid['semi_minor']
-
-    # Convert geodetic latitude to geocentric latitude
-    phi_bar = geodetic2geocentric(phi, semi_major=semi_major, semi_minor=semi_minor)
-    # phi_bar = phi
-
-    # Calculate trigonometric values
+    if phi is None and vartheta is None:
+        raise ValueError('Either phi or vartheta must be provided')
+    
+    if lambd is None and vartheta is None:
+        raise ValueError('Please provide lambd')
+    
+    if vartheta is not None: 
+        phi_bar = vartheta
+    elif phi is not None:
+        _, phi_bar, _ = geodetic2spherical(phi, lambd, ellipsoid, height=0)
+        phi_bar = degrees(phi_bar)
+    
+    # sine (u) and cosine (t) terms
     t = cos(radians(phi_bar))
     u = sin(radians(phi_bar))
 
@@ -56,14 +60,14 @@ def ALF(phi, nmax=60, ellipsoid='wgs84'):
     # Recursive computation of Pnm
     for n in range(2, nmax + 1):
         for m in range(0, n):
-            a_nm = sqrt((2.0 * n - 1.0) * (2.0 * n + 1.0) / ((n - m) * (n + m)))
-            b_nm = 0.0
+            a_nm = sqrt((2. * n - 1.) * (2. * n + 1.0) / ((n - m) * (n + m)))
+            b_nm = 0.
             if n - m - 1 >= 0:
-                b_nm = sqrt((2.0 * n + 1.0) * (n + m - 1.0) * (n - m - 1.0) / ((n - m) * (n + m) * (2.0 * n - 3.0)))
+                b_nm = sqrt((2. * n + 1.) * (n + m - 1.) * (n - m - 1.) / ((n - m) * (n + m) * (2. * n - 3.)))
             Pnm[n, m] = a_nm * t * Pnm[n - 1, m] - b_nm * Pnm[n - 2, m]
 
         # Sectoral harmonics (n = m)
-        Pnm[n, n] = u * sqrt((2.0 * n + 1.0) / (2.0 * n)) * Pnm[n - 1, n - 1]
+        Pnm[n, n] = u * sqrt((2. * n + 1.) / (2. * n)) * Pnm[n - 1, n - 1]
 
     return Pnm
     
@@ -105,4 +109,64 @@ def legendre_poly(theta=None, t=None, nmax=60):
         Pn[n] = ( (2*n-1)*t*Pn[n-1] - (n-1)*Pn[n-2] ) / n
     
     return Pn
+
+
+# def ALF(phi=None, vartheta=None, nmax=60, ellipsoid='wgs84'):
+#     '''
+#     Compute associated Legendre functions
+
+#     Parameters
+#     ----------
+#     phi       : geodetic latitude (degrees)
+#     vartheta  : colatitude        (degrees)
+#     nmax      : maximum degree of expansion
+#     ellipsoid : reference ellipsoid ('wgs84' or 'grs80')
+    
+#     Returns
+#     -------
+#     Pnm       : Fully normalized Associated Legendre functions
+    
+#     References
+#     ----------
+#     (1) Holmes and Featherstone (2002): A unified approach to the Clenshaw 
+#     summation and the recursive computation of very high degree and order 
+#     normalised associated Legendre functions (Eqs. 11 and 12)
+#     '''
+
+#     if phi is None and vartheta is None:
+#         raise ValueError('Either phi or vartheta must be provided')
+
+#     if phi is not None and vartheta is not None: 
+#         phi_bar = vartheta
+#     elif phi is not None:
+#         phi_bar = geodetic2geocentric(phi, ellipsoid=ellipsoid)
+#     elif vartheta is not None:
+#         phi_bar = vartheta
+
+#     # Calculate trigonometric values
+#     t = cos(radians(phi_bar))
+#     u = sin(radians(phi_bar))
+
+#     # Initialize the Pnm array
+#     Pnm = zeros((nmax + 1, nmax + 1))
+#     Pnm[0, 0] = 1.0
+
+#     # Initialize first few values
+#     if nmax >= 1:
+#         Pnm[1, 0] = sqrt(3.0) * t
+#         Pnm[1, 1] = sqrt(3.0) * u
+
+#     # Recursive computation of Pnm
+#     for n in range(2, nmax + 1):
+#         for m in range(0, n):
+#             a_nm = sqrt((2.0 * n - 1.0) * (2.0 * n + 1.0) / ((n - m) * (n + m)))
+#             b_nm = 0.0
+#             if n - m - 1 >= 0:
+#                 b_nm = sqrt((2.0 * n + 1.0) * (n + m - 1.0) * (n - m - 1.0) / ((n - m) * (n + m) * (2.0 * n - 3.0)))
+#             Pnm[n, m] = a_nm * t * Pnm[n - 1, m] - b_nm * Pnm[n - 2, m]
+
+#         # Sectoral harmonics (n = m)
+#         Pnm[n, n] = u * sqrt((2.0 * n + 1.0) / (2.0 * n)) * Pnm[n - 1, n - 1]
+
+#     return Pnm
     
