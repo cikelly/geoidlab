99d579ae00261defd1ad156b57a4469a5877deed
Caleb Kelly <calebaid2000@gmail.com>
2024-08-05 12:58:12 +0800

    mean tide to free tide

--- src/tide_system.py
+++ src/tide_system.py
@@ -4,19 +4,38 @@
 # Author: Caleb Kelly  (2024)                              #
 ############################################################
 import pandas as pd
+import numpy as np
 
 class TideSystemConverter:
+    '''
+    Convert between different permanet tide systems
+    
+    Parameters
+    ----------
+    path_to_data      : path to data file
+    data              : numpy array or Pandas DataFrame or dict
+                        (lon, lat, elevation, gravity)
+    
+    Methods
+    -------
+    read_file         : Reads the file containing gravity data and returns 
+                        a Pandas DataFrame
+    gravity_mean2free : Convert gravity data in mean tide to free tide system
+    
+    Notes
+    -----
+    1. Arrange your data in the order: lon, lat, elevation, gravity
+    2. Supported file formats: .csv, .txt, .xlsx, .xls
+    3. If both path_to_data and data are provided, data will be used
+    '''
     def __init__(self, path_to_data=None, data=None):
         self.path_to_data = path_to_data
-        self.data = data if data is not None else self.read_file(path_to_data)
+        # self.data = data if data is not None else self.read_file()
+        self.data = data
     
-    def read_file(file_path):
+    def read_file(self):
         '''
         Read file containing gravity data
-        
-        Parameters
-        ----------
-        file_path : Path to the file
         
         Returns
         -------
@@ -28,6 +47,10 @@ class TideSystemConverter:
             'h': ['h', 'height', 'z', 'elevation', 'elev'],
             'gravity': ['gravity', 'g', 'acceleration', 'grav']
         }
+        file_path = self.path_to_data
+        
+        if file_path is None:
+            raise ValueError('File path not specified')
         
         if file_path.endswith('.csv'):
             df = pd.read_csv(file_path)
@@ -37,10 +60,15 @@ class TideSystemConverter:
             df = pd.read_csv(file_path, delimiter='\t')
         else:
             raise ValueError('Unsupported file format')
-    
-    def gravity_free2mean(self):
+
+        # Rename columns to standardized names
+        df = df.rename(columns=lambda col: next((key for key, values in column_mapping.items() if col.lower() in values), col))
+        
+        return df
+
+    def gravity_mean2free(self):
         '''
-        Convert free tide to mean tide system for gravity data
+        Convert gravity data in mean tide system to free tide system
         
         References
         ----------
@@ -52,57 +80,86 @@ class TideSystemConverter:
                                 http://www.springerlink.com/index/10.1007/BF02520477
         '''
         # Code for conversion goes here
-        pass
-    
-    def geoid_free2mean(self):
-        '''
-        Convert free tide to mean tide system for geoid data
+        if self.data is None and self.path_to_data is None:
+            raise ValueError('Provide Data or path to data.')
+
+        if self.data is not None:
+            print('Data provided. Converting to free tide system.')
+            data = self.data
+            
+            if isinstance(data, pd.DataFrame):
+                # Rename columns to lon, lat, h, and gravity
+                data.columns = ['lon', 'lat', 'h', 'gravity']
+            elif isinstance(data, np.ndarray):
+                # Create a Pandas DataFrame from the numpy array
+                data = pd.DataFrame(data, columns=['lon', 'lat', 'h', 'gravity'])
+            elif isinstance(data, dict):
+                # Create a Pandas DataFrame from the dictionary
+                data = pd.DataFrame(data)
+                data.columns = ['lon', 'lat', 'h', 'gravity']     
+        elif self.path_to_data is not None:
+            print('Path to data provided. Reading data from file.')
+            # Read data from file
+            data = self.read_file()
         
-        References
-        ----------
-        1. Tenzer et al. (2010): Assessment of the LVD offsets for the normal-orthometric 
-                                heights and different permanent tide systems—A case study of New Zealand
-                                http://link.springer.com/10.1007/s12518-010-0038-5
+        # Convert gravity data to free tide system
+        d = 1.53
+        data['gravity_free'] = data['gravity'] * 1000 - d * (-30.4 + 91.2 * np.sin(np.radians(data['lat'])) ** 2) # uGal
+        data['gravity_free'] = data['gravity_free'] * 1e-3 # mGal
+        # Convert elevation data to free tide system
+        k = 0.3
+        hc = 0.6
+        data['h_free'] = data['h'] + (1 + k - hc) * (-0.198 * (3/2 * np.sin(np.radians(data['lat'])) ** 2 - 1/2)) # m
+        
+        return data
+    
+    # TO DO: Add other methods for other tide systems
+    
+    
+    # def geoid_mean2free(self):
+    #     '''
+    #     Convert free tide to mean tide system for geoid data
+        
+    #     References
+    #     ----------
+    #     1. Tenzer et al. (2010): Assessment of the LVD offsets for the normal-orthometric 
+    #                             heights and different permanent tide systems—A case study of New Zealand
+    #                             http://link.springer.com/10.1007/s12518-010-0038-5
                                 
-        2. Ekman (1989)        : Impacts of geodynamic phenomena on systems for height and gravity
-                                http://www.springerlink.com/index/10.1007/BF02520477
-        '''
-        # Code for conversion goes here
-        pass
+    #     2. Ekman (1989)        : Impacts of geodynamic phenomena on systems for height and gravity
+    #                             http://www.springerlink.com/index/10.1007/BF02520477
+    #     '''
+    #     # Code for conversion goes here
+    #     pass
     
-
+    # def gravity_free2mean(self):
+    #     '''
+    #     Convert free tide to mean tide system for gravity data
+        
+    #     References
+    #     ----------
+    #     1. Tenzer et al. (2010): Assessment of the LVD offsets for the normal-orthometric 
+    #                             heights and different permanent tide systems—A case study of New Zealand
+    #                             http://link.springer.com/10.1007/s12518-010-0038-5
+                                
+    #     2. Ekman (1989)        : Impacts of geodynamic phenomena on systems for height and gravity
+    #                             http://www.springerlink.com/index/10.1007/BF02520477
+    #     '''
+    #     # Code for conversion goes here
+    #     pass
     
-
-    
-    # Update column names to lon, lat, H, and gravity
-# def read_file(file_path):
-#     # Define possible column names for each required column
-#     column_mapping = {
-#         'lon': ['lon', 'x'],
-#         'lat': ['lat', 'y'],
-#         'h': ['h', 'height', 'z'],
-#         'gravity': ['gravity', 'g', 'acceleration']
-#     }
-    
-#     # Read the file based on its extension
-#     if file_path.endswith('.csv'):
-#         df = pd.read_csv(file_path)
-#     elif file_path.endswith('.xlsx'):
-#         df = pd.read_excel(file_path)
-#     elif file_path.endswith('.txt'):
-#         df = pd.read_csv(file_path, delimiter='\t')
-#     else:
-#         raise ValueError('Unsupported file format')
-    
-#     # Create a new DataFrame with standardized column names
-#     standardized_df = pd.DataFrame()
-    
-#     for standard_col, possible_cols in column_mapping.items():
-#         for col in possible_cols:
-#             if col in df.columns:
-#                 standardized_df[standard_col] = df[col]
-#                 break
-#         else:
-#             raise ValueError(f"Required column '{standard_col}' not found in the file")
-    
-#     return standardized_df
+    # def geoid_free2mean(self):
+    #     '''
+    #     Convert free tide to mean tide system for geoid data
+        
+    #     References
+    #     ----------
+    #     1. Tenzer et al. (2010): Assessment of the LVD offsets for the normal-orthometric 
+    #                             heights and different permanent tide systems—A case study of New Zealand
+    #                             http://link.springer.com/10.1007/s12518-010-0038-5
+                                
+    #     2. Ekman (1989)        : Impacts of geodynamic phenomena on systems for height and gravity
+    #                             http://www.springerlink.com/index/10.1007/BF02520477
+    #     '''
+    #     # Code for conversion goes here
+    #     pass
