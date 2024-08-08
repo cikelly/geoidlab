d1b979b0ae3174bbbba390712446ac6ea7ee2970
Caleb Kelly <calebaid2000@gmail.com>
2024-08-08 12:11:55 +0800

    Added function to calculate gravity disturbance

--- src/ggm_tools.py
+++ src/ggm_tools.py
@@ -11,6 +11,7 @@ from numba import jit
 import coordinates as co
 import numpy as np
 import pandas as pd
+from numba_progress import ProgressBar
 
 import gravity
 import tide_system
@@ -170,14 +171,6 @@ def zero_degree_term(lat, shc=None, GM=None, geoid=None, ellipsoid='wgs84'):
     return N
 
 
-
-
-
-from numba import jit
-import numpy as np
-import pandas as pd
-from numba_progress import ProgressBar
-
 @jit(nopython=True)
 def compute_gravity_chunk(Cnm, Snm, lon, a, GM, r, Pnm, n, Dg):
     '''
@@ -272,6 +265,99 @@ def gravity_anomaly(shc, grav_data=None, ellipsoid='wgs84', nmax=300):
     
     return pd.Series(Dg)
 
+@jit(nopython=True)
+def compute_disturbance_chunk(Cnm, Snm, lon, a, GM, r, Pnm, n, dg):
+    '''
+    Compute a chunk of gravity disturbance for a specific degree n using Numba for optimization
+
+    Parameters
+    ----------
+    Cnm, Snm : Spherical Harmonic Coefficients (2D arrays)
+    lon      : Longitude (1D arrays)
+    a        : Reference radius 
+    GM       : Gravitational constant times the mass of the Earth
+    r        : Radial distance (1D array)
+    Pnm      : Associated Legendre functions (3D array)
+    n        : Specific degree
+    dg       : Gravity disturbance array to update
+
+    Returns
+    -------
+    Updated dg array with computed values for degree n
+    '''
+    sum = np.zeros(len(lon))
+    for m in range(n+1):
+        sum += (Cnm[n, m] * np.cos(m * lon) + Snm[n, m] * np.sin(m * lon)) * Pnm[:, n, m]
+    dg += (n + 1) * (a / r) ** n * sum
+
+    return dg
+
+def gravity_disturbance(shc, grav_data=None, ellipsoid='wgs84', nmax=300):
+    '''
+    Wrapper function to handle data and call the Numba-optimized function
+    
+    Parameters
+    ----------
+    shc       : Spherical Harmonic Coefficients (output of icgem.read_icgem() and shtools.replace_zonal_harmonics())
+    grav_data : Gravity data with columns lon, lat, and elevation: lat and lon units: degrees
+    ellipsoid : Reference ellipsoid
+    nmax      : Maximum spherical harmonic degree of expansion
+    
+    Returns
+    -------
+    dg        : Gravity anomaly (mGal)
+    
+    Notes
+    -----
+    1. Torge, Müller, & Pail (2023): Geodesy, Eq. 6.35b, p.297
+    2. Please ensure that you have called shtools.replace_zonal_harmonics() on shc before passing it to gravity_anomaly()
+    '''        
+    if grav_data is None:
+        raise ValueError('Provide data with columns lon, lat, and elevation in order')
+    else:
+        if isinstance(grav_data, np.ndarray):
+            lon = grav_data[:,0]
+            lat = grav_data[:,1]
+            try:
+                h = grav_data[:,2]
+            except IndexError:
+                print('Looks like there is no elevation column. Setting elevation to 0')
+                h = np.zeros(len(lat))
+        elif isinstance(grav_data, pd.DataFrame):
+            lon_column = [col for col in grav_data.columns if pd.Series(col).str.contains('lon', case=False).any()][0]
+            lat_column = [col for col in grav_data.columns if pd.Series(col).str.contains('lat', case=False).any()][0]
+            lon = grav_data[lon_column].values
+            lat = grav_data[lat_column].values
+            try:
+                elev_column = [col for col in grav_data.columns if pd.Series(col).str.contains(r'elev|height', case=False).any()][0]
+                h = grav_data[elev_column].values
+            except IndexError:
+                print('Looks like there is no elevation column. Setting elevation to 0')
+                h = np.zeros(len(lat))
+                
+    r, vartheta, _ = co.geodetic2spherical(phi=lat, lambd=lon, height=h, ellipsoid=ellipsoid)
+        
+    lon = np.radians(lon)
+    lat = np.radians(lat)
+    
+    Pnm = ALFsGravityAnomaly(vartheta=vartheta, nmax=nmax, ellipsoid=ellipsoid)
+    
+    Cnm = np.array(shc['Cnm'])
+    Snm = np.array(shc['Snm'])
+    a   = shc['a']
+    Gm  = shc['GM']
+    
+    dg = np.zeros(len(lon))
+
+    # Initialize progress bar
+    with ProgressBar(total=nmax - 1, desc='Calculating Gravity Disturbance') as pbar:
+        for n in range(2, nmax + 1):
+            dg = compute_disturbance_chunk(Cnm, Snm, lon, a, Gm, r, Pnm, n, dg)
+            pbar.update(1)
+    
+    dg = Gm / r ** 2 * dg * 10**5  # mGal
+    
+    return pd.Series(dg)
 
 # def gravity_anomaly(shc, grav_data=None, ellipsoid='wgs84', nmax=300):
 #     '''
