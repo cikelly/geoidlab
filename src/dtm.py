37f8d1c906437db55b03b149c159cbea838c5d0a
Caleb Kelly <calebaid2000@gmail.com>
2024-08-09 15:10:11 +0800

    Update `calculate_height`

--- src/dtm.py
+++ src/dtm.py
@@ -6,9 +6,14 @@
 
 import lzma
 import os
+import constants
 
 import numpy as np
+import pandas as pd
 
+from legendre import ALFsGravityAnomaly
+from numba import jit
+from numba_progress import ProgressBar
 # from tqdm import tqdm
 
 class DigitalTerrainModel:
@@ -47,8 +52,69 @@ class DigitalTerrainModel:
             if n <= self.nmax+1 and m <= self.nmax+1:
                 HCnm[n,m] = float(line[2].replace('D', 'E'))
                 HSnm[n,m] = float(line[3].replace('D', 'E'))
-            
-        return self.HCnm, self.HSnm
+            self.HCnm = HCnm
+            self.HSnm = HSnm
+        # return self.HCnm, self.HSnm
 
-    def calculate_height(self):
-        pass
+    @jit(nopython=True)
+    def compute_height_chunk(HCnm, HSnm, lon, lat, n, Pnm):
+        '''
+        Compute a chunk of heights for a specific degree n using Numba for optimization
+
+        Parameters
+        ----------
+        HCnm, HSnm : Spherical Harmonic Coefficients (2D arrays)
+        lon        : Longitude (1D arrays)
+        lat        : Latitude (1D arrays)
+        n          : Specific degree
+        Pnm        : Associated Legendre functions (3D array)
+
+        Returns
+        -------
+        H          : Heights (1D array)
+        '''
+        H = np.zeros(len(lon))
+        for m in range(n+1):
+            H += (HCnm[n, m] * np.cos(m * lon) + HSnm[n, m] * np.sin(m * lon)) * Pnm[:, n, m]
+        # H = sum  
+        
+        return H  
+            
+    def calculate_height(self, lon, lat, Pnm=None, ellipsoid='wgs84'):
+        '''
+        Wrapper function to handle data and call the Numba-optimized function
+        
+        Parameters
+        ----------
+        lon        : geodetic/geographic longitude of point(s) of interest (degrees)
+        lat        : geodetic/geographic latitude of point(s) of interest  (degrees)
+        nmax       : maximum spherical harmonic degree of expansion
+        Pnm        : fully normalized associated Legendre functions
+
+        Returns
+        -------
+        Hdtm       : Synthesized height
+
+        References
+        ----------
+        1. Sanso and Sideris (2013): Geoid Determination Theory and Methods, Chapter 8. (Eq. 8.79)
+        '''
+        if Pnm is None:
+            Pnm = ALFsGravityAnomaly(phi=lat, lambd=lon, nmax=self.nmax)
+        
+        if isinstance(lon, pd.Series):
+            lon = lon.values
+        if isinstance(lat, pd.Series):
+            lat = lat.values
+        
+        lon = np.radians(lon)
+        lat = np.radians(lat)
+        
+        H = np.zeros(lon)
+        
+        with ProgressBar(total=self.nmax-1, desc='Synthesizing heights from DTM') as pbar:
+            for n in range(2, self.nmax+1):
+                H += self.compute_height_chunk(self.HCnm, self.HSnm, lon, lat, n, Pnm)
+                pbar.update(1)
+        
+        return H
