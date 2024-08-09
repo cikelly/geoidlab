bdc82ec50588910b2995c1938f4055f2181e2e92
Caleb Kelly <calebaid2000@gmail.com>
2024-08-09 12:18:35 +0800

    Digital Terrain Modeling

---
+++ src/dtm.py
@@ -0,0 +1,65 @@
+############################################################
+# Utilities for calculating reference geoid                #
+# Copyright (c) 2024, Caleb Kelly                          #
+# Author: Caleb Kelly  (2024)                              #
+############################################################
+
+import lzma
+import os
+
+import numpy as np
+
+# from tqdm import tqdm
+
+
+
+class DigitalTerrainModel:
+    def __init__(self, model_name=None, nmax=300):
+        self.name = model_name
+        self.nmax = nmax
+        
+        if self.name is None:
+            script_dir = os.path.dirname(__file__)
+            self.name = os.path.join(script_dir, 'data', 'DTM2006.xz')
+            print(f'Using compressed file in src/data directory ...')
+            
+    def read_dtm2006_lzma(self):
+        '''
+        Read DTM data stored as compressed LZMA file
+        '''
+        with lzma.open(self.name, 'rt') as f:
+            dtm = f.readlines()
+
+        HCnm = np.zeros((self.nmax+1, self.nmax+1))
+        HSnm = np.zeros((self.nmax+1, self.nmax+1))
+            
+            
+        for line in dtm:
+            line = line.split()
+            
+            n = int(line[0])
+            m = int(line[1])
+
+            if n > self.nmax:
+                break
+            if n <= self.nmax+1 and m <= self.nmax+1:
+                HCnm[n,m] = float(line[2].replace('D', 'E'))
+                HSnm[n,m] = float(line[3].replace('D', 'E'))
+            
+        return HCnm, HSnm
+        
+        # for line in tqdm(range(len(dtm))):
+        #     line = line.split()
+        #     n = int(line[0])
+        #     m = int(line[1])
+        #     HCnm[n,m] = float(line[2].replace('D', 'E'))
+        #     HSnm[n,m] = float(line[3].replace('D', 'E'))
+
+        # return HCnm, HSnm
+    
+    def read_dtm2006(self):
+        '''
+        Read DTM data stored as uncompressed binary file
+        '''
+        with open(f'data/{self.name}', 'r') as f:
+            dtm = f.readlines()
