6782b849a3ce252a1b973e3d848fb0ce19f31751
Caleb Kelly <calebaid2000@gmail.com>
2024-08-11 13:53:35 +0800

    Delete unused arguments

--- src/ggm_tools.py
+++ src/ggm_tools.py
@@ -1,3 +1,9 @@
+############################################################
+# Utilities for calculating reference geoid                #
+# Copyright (c) 2024, Caleb Kelly                          #
+# Author: Caleb Kelly  (2024)                              #
+############################################################
+
 import icgem
 import shtools
 import constants
