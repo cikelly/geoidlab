ef388c957042ce68671d7bffd0091311ea65e744
Caleb Kelly <calebaid2000@gmail.com>
2024-08-25 21:40:59 +0800

    Updates including typing and `km2deg` method

--- src/easy_pygeoid/terrain.py
+++ src/easy_pygeoid/terrain.py
@@ -9,14 +9,20 @@ from easy_pygeoid.coordinates import geodetic2cartesian
 
 class TerrainQuantities:
     '''
+    Compute terrain quantities for use in geoid modeling
+        - terrain correction
+        - residual terrain modeling (RTM)
+        - indirect effect
     '''
     def __init__(
         self, 
-        ref_topo, 
-        sim_topo, 
-        Lon, Lat,
-        sub_grid=[0,0,0,0],
-        ellipsoid=constants.wgs84()
+        ref_topo: float, 
+        sim_topo: float, 
+        Lon:float, 
+        Lat:float,
+        radius:float=100.,
+        sub_grid=None,
+        ellipsoid:str='wgs84'
     ) -> None:
         '''
         Initialize the TerrainQuantities class for terrain modeling
@@ -26,6 +32,7 @@ class TerrainQuantities:
         ref_topo  : (2D array) Reference topography
         sim_topo  : (2D array) Simulated (smooth) topography
         Lon, Lat  : (2D array) Longitude and latitude of ref_topo and sim_topo
+        radius    : Integration radius in kilometers
         sub_grid  : (4-tuple) Subgrid over which to compute terrain effects
                         - In the form W, E, S, N
         ellipsoid : Reference ellipsoid
@@ -43,10 +50,24 @@ class TerrainQuantities:
         self.ellipsoid         = ellipsoid
         self.sub_grid          = sub_grid
         self.nrows, self.ncols = ref_topo.shape
+        self.radius            = radius * 1000 # meters
         
+        # Define sub-grid
+        if self.sub_grid is None:
+            self.radius_deg = self.km2deg((self.radius / 1000))
+        
+        
+        self.rows, self.cols = self.ref_topo.shape
         min_lon, max_lon, min_lat, max_lat = sub_grid
         
         self.sim_topo[self.sim_topo < 0] = 0
+    
+    @staticmethod
+    def km2deg(km:float) -> float:
+        '''
+        Convert kilometers to degrees
+        '''
+        return km / 111.11
     
     def terrain_correction(self):
         pass
