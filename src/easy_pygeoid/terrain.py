1ad09eea954d0a9baad90d481e62c72af80682db
Caleb Kelly <calebaid2000@gmail.com>
2024-08-26 00:06:56 +0800

    Update docstring

--- src/easy_pygeoid/terrain.py
+++ src/easy_pygeoid/terrain.py
@@ -20,13 +20,13 @@ class TerrainQuantities:
     '''
     def __init__(
         self, 
-        ref_topo: float, 
-        sim_topo: float, 
-        Lon:float, 
-        Lat:float,
-        radius:float=100.,
+        Lon: float, 
+        Lat: float,
+        ref_topo: float=None, 
+        sim_topo: float=None, 
+        radius: float=100.,
         sub_grid=None,
-        ellipsoid:str='wgs84'
+        ellipsoid: str='wgs84'
     ) -> None:
         '''
         Initialize the TerrainQuantities class for terrain modeling
@@ -41,6 +41,15 @@ class TerrainQuantities:
                         - In the form W, E, S, N
         ellipsoid : Reference ellipsoid
 
+        Returns
+        -------
+        None
+        
+        Reference
+        ---------
+        Wichiencharoen (1982): The Indirect Effects On The Computation of Geoid Undulations
+        https://ntrs.nasa.gov/citations/19830016735
+        
         Notes
         -----
         1. ref_topo is the reference topography from a digital elevation model
@@ -57,18 +66,27 @@ class TerrainQuantities:
         self.nrows, self.ncols = ref_topo.shape
         self.radius            = radius * 1000 # meters
         
+        if self.ref_topo is None and self.sim_topo is None:
+            raise ValueError('Either ref_topo and sim_topo must be provided')
+        
         # Define sub-grid
         if self.sub_grid is None:
             print(f'Defining sub-grid based on integration radius: {radius} km')
             self.radius_deg = self.km2deg((self.radius / 1000))
-            
+            min_lat = np.round(np.min(Lat) + self.radius_deg)
+            max_lat = np.round(np.max(Lat) - self.radius_deg)
+            min_lon = np.round(np.min(Lon) + self.radius_deg)
+            max_lon = np.round(np.max(Lon) - self.radius_deg)
+            self.sub_grid = (min_lon, max_lon, min_lat, max_lat)
+        
+        # Set ocean areas to zero
+        if self.sim_topo is not None:
+            self.sim_topo[self.sim_topo < 0] = 0
+        if self.ref_topo is not None:
+            self.ref_topo[self.ref_topo < 0] = 0
+
         
         
-        # self.rows, self.cols = self.ref_topo.shape
-        # min_lon, max_lon, min_lat, max_lat = sub_grid
-        
-        self.sim_topo[self.sim_topo < 0] = 0
-    
     @staticmethod
     def km2deg(km:float, radius:float=6371.) -> float:
         '''
@@ -82,6 +100,12 @@ class TerrainQuantities:
         Returns
         -------
         deg       : degrees
+        
+        Notes
+        -----
+        1. Using the radius of the sphere is more accurate than 2.
+        2. km / 111.11 is a reasonable approximation, and works well in practice.
+        3. The approach used here is the same as MATLAB's km2deg function
         '''
         rad = km / radius
         deg = rad * 180 / np.pi
