89245988e2619591163ff7704ca91f5cf2bb419c
Caleb Kelly <calebaid2000@gmail.com>
2025-03-02 11:59:39 +0000

    Refactor process_chunk method in DigitalTerrainModel for improved readability and maintainability

--- src/easy_pygeoid/dtm.py
+++ src/easy_pygeoid/dtm.py
@@ -15,21 +15,6 @@ from multiprocessing import Pool, cpu_count
 import numpy as np
 
 
-def process_chunk(args) -> np.ndarray:
-    self, lon_chunk, lat_chunk, height_chunk, nmax, ellipsoid, leg_progress, chunk_idx = args
-    _, theta, _ = co.geodetic2spherical(phi=lat_chunk, lambd=lon_chunk, ellipsoid=ellipsoid, height=height_chunk)
-    _lambda = np.radians(lon_chunk)
-    m = np.arange(nmax + 1)
-    mlambda = m[:, np.newaxis] * _lambda
-    cosm = np.cos(mlambda)
-    sinm = np.sin(mlambda)
-    
-    Pnm = ALFsGravityAnomaly(vartheta=theta, nmax=nmax, ellipsoid=ellipsoid, show_progress=leg_progress)
-    
-    # H = 
-
-    return compute_harmonic_sum(Pnm, self.HCnm, self.HSnm, cosm, sinm)
-
 class DigitalTerrainModel:
     def __init__(self, model_name=None, nmax=2190, ellipsoid='wgs84') -> None:
         '''
@@ -60,6 +45,22 @@ class DigitalTerrainModel:
             with open(self.name, 'r') as f:
                 self.dtm = f.readlines() # self.dtm is the DTM2006 text file
                 
+
+    @staticmethod
+    def process_chunk(args) -> np.ndarray:
+        self, lon_chunk, lat_chunk, height_chunk, nmax, ellipsoid, leg_progress, chunk_idx = args
+        _, theta, _ = co.geodetic2spherical(phi=lat_chunk, lambd=lon_chunk, ellipsoid=ellipsoid, height=height_chunk)
+        _lambda = np.radians(lon_chunk)
+        m = np.arange(nmax + 1)
+        mlambda = m[:, np.newaxis] * _lambda
+        cosm = np.cos(mlambda)
+        sinm = np.sin(mlambda)
+        
+        Pnm = ALFsGravityAnomaly(vartheta=theta, nmax=nmax, ellipsoid=ellipsoid, show_progress=leg_progress)
+        
+        # H = 
+
+        return compute_harmonic_sum(Pnm, self.HCnm, self.HSnm, cosm, sinm)
 
     def read_dtm2006(self) -> None:
         '''
@@ -124,196 +125,8 @@ class DigitalTerrainModel:
         
         Pnm = ALF(vartheta=theta, nmax=self.nmax, ellipsoid=self.ellipsoid)
         H = np.sum((self.HCnm * Pnm) @ cosm + (self.HSnm * Pnm) @ sinm)
+        
         return float(H)
-    
-    # def dtm2006_height(
-    #     self,
-    #     lon: np.ndarray,
-    #     lat: np.ndarray,
-    #     chunk_size: int = 100,
-    #     leg_progress: bool = False,
-    #     height: np.ndarray = None
-    # ) -> np.ndarray:
-    #     '''
-    #     Compute heights from DTM2006.0 spherical harmonic coefficients
-        
-    #     Parameters
-    #     ----------
-    #     lon         : geodetic longitude
-    #     lat         : geodetic latitude
-    #     chunk_size  : number of points to process at a time
-    #     leg_progress: show progress bar for Legendre polynomial computation
-    #     height      : height above ellipsoid (optional)
-        
-    #     Returns
-    #     -------
-    #     H           : synthesized height
-    #     '''
-    #     import numpy as np
-    #     from tqdm import tqdm
-    #     import easy_pygeoid.coordinates as co
-    #     from easy_pygeoid.legendre import ALFsGravityAnomaly
-
-    #     lon = np.asarray(lon)
-    #     lat = np.asarray(lat)
-        
-    #     if lon.shape != lat.shape:
-    #         raise ValueError('lon and lat must have the same shape')
-
-    #     input_shape = lon.shape
-    #     lon_flat = lon.flatten()
-    #     lat_flat = lat.flatten()
-    #     height_flat = np.zeros_like(lon_flat) if height is None else height.flatten()
-    #     num_points = len(lon_flat)
-        
-    #     if num_points == 1:
-    #         return self.dtm2006_height_point(lon_flat[0], lat_flat[0])
-
-    #     H_flat = np.zeros(num_points)
-        
-    #     if num_points <= chunk_size:
-    #         r, theta, _ = co.geodetic2spherical(
-    #             phi=lat_flat, 
-    #             lambd=lon_flat, 
-    #             ellipsoid=self.ellipsoid,
-    #             height=height_flat
-    #         )
-    #         _lambda = np.radians(lon_flat)
-    #         m = np.arange(self.nmax + 1)
-    #         mlambda = m[:, np.newaxis] * _lambda
-    #         cosm = np.cos(mlambda)  # (nmax+1, num_points)
-    #         sinm = np.sin(mlambda)  # (nmax+1, num_points)
-            
-    #         Pnm = ALFsGravityAnomaly(
-    #             vartheta=theta, 
-    #             nmax=self.nmax, 
-    #             ellipsoid=self.ellipsoid,
-    #             show_progress=leg_progress
-    #         )
-            
-    #         H_flat = (np.einsum('inm,nm,im->i', Pnm, self.HCnm, cosm.T) + 
-    #                 np.einsum('inm,nm,im->i', Pnm, self.HSnm, sinm.T))
-    #     else:
-    #         for start in tqdm(range(0, num_points, chunk_size), desc='Computing chunks'):
-    #             end = min(start + chunk_size, num_points)
-    #             lon_chunk = lon_flat[start:end]
-    #             lat_chunk = lat_flat[start:end]
-    #             height_chunk = height_flat[start:end]
-                
-    #             r, theta, _ = co.geodetic2spherical(
-    #                 phi=lat_chunk, 
-    #                 lambd=lon_chunk, 
-    #                 ellipsoid=self.ellipsoid,
-    #                 height=height_chunk
-    #             )
-    #             _lambda = np.radians(lon_chunk)
-    #             m = np.arange(self.nmax + 1)
-    #             mlambda = m[:, np.newaxis] * _lambda
-    #             cosm = np.cos(mlambda)  # (nmax+1, chunk_size)
-    #             sinm = np.sin(mlambda)  # (nmax+1, chunk_size)
-                
-    #             Pnm = ALFsGravityAnomaly(
-    #                 vartheta=theta, 
-    #                 nmax=self.nmax, 
-    #                 ellipsoid=self.ellipsoid,
-    #                 show_progress=leg_progress
-    #             )
-                
-    #             H_flat[start:end] = (np.einsum('inm,nm,im->i', Pnm, self.HCnm, cosm.T) + 
-    #                                 np.einsum('inm,nm,im->i', Pnm, self.HSnm, sinm.T))
-        
-    #     return H_flat.reshape(input_shape)
-
-    # def dtm2006_height(
-    #     self,
-    #     lon: np.ndarray,
-    #     lat: np.ndarray,
-    #     chunk_size: int = 100,
-    #     leg_progress: bool = False,
-    #     height: np.ndarray = None
-    # ) -> np.ndarray:
-    #     '''
-    #     Compute heights from DTM2006.0 spherical harmonic coefficients
-        
-    #     Parameters
-    #     ----------
-    #     lon         : geodetic longitude
-    #     lat         : geodetic latitude
-    #     chunk_size  : number of points to process at a time
-    #     leg_progress: show progress bar for Legendre polynomial computation
-    #     height      : height above ellipsoid (optional)
-        
-    #     Returns
-    #     -------
-    #     H           : synthesized height
-    #     '''
-    #     lon = np.asarray(lon)
-    #     lat = np.asarray(lat)
-        
-    #     if lon.shape != lat.shape:
-    #         raise ValueError('lon and lat must have the same shape')
-
-    #     input_shape = lon.shape
-    #     lon_flat = lon.flatten()
-    #     lat_flat = lat.flatten()
-    #     height_flat = np.zeros_like(lon_flat) if height is None else height.flatten()
-    #     num_points = len(lon_flat)
-        
-    #     if num_points == 1:
-    #         return self.dtm2006_height_point(lon_flat[0], lat_flat[0])
-
-    #     H_flat = np.zeros(num_points)
-        
-    #     if num_points <= chunk_size:
-    #         r, theta, _ = co.geodetic2spherical(
-    #             phi=lat_flat, 
-    #             lambd=lon_flat, 
-    #             ellipsoid=self.ellipsoid,
-    #             height=height_flat
-    #         )
-    #         _lambda = np.radians(lon_flat)  # (num_points,)
-    #         m = np.arange(self.nmax + 1)    # (nmax+1,)
-    #         mlambda = m[:, np.newaxis] * _lambda  # (nmax+1, num_points)
-    #         cosm = np.cos(mlambda)                # (nmax+1, num_points)
-    #         sinm = np.sin(mlambda)                # (nmax+1, num_points)
-            
-    #         Pnm = ALFsGravityAnomaly(
-    #             vartheta=theta, 
-    #             nmax=self.nmax, 
-    #             ellipsoid=self.ellipsoid,
-    #             show_progress=leg_progress
-    #         )  # (num_points, nmax+1, nmax+1)
-            
-    #         H_flat = compute_harmonic_sum(Pnm, self.HCnm, self.HSnm, cosm, sinm)
-    #     else:
-    #         for start in tqdm(range(0, num_points, chunk_size), desc='Computing chunks'):
-    #             end = min(start + chunk_size, num_points)
-    #             lon_chunk = lon_flat[start:end]
-    #             lat_chunk = lat_flat[start:end]
-    #             height_chunk = height_flat[start:end]
-                
-    #             r, theta, _ = co.geodetic2spherical(
-    #                 phi=lat_chunk, 
-    #                 lambd=lon_chunk, 
-    #                 ellipsoid=self.ellipsoid,
-    #                 height=height_chunk
-    #             )
-    #             _lambda = np.radians(lon_chunk)  # (chunk_size,)
-    #             m = np.arange(self.nmax + 1)     # (nmax+1,)
-    #             mlambda = m[:, np.newaxis] * _lambda  # (nmax+1, chunk_size)
-    #             cosm = np.cos(mlambda)                # (nmax+1, chunk_size)
-    #             sinm = np.sin(mlambda)                # (nmax+1, chunk_size)
-                
-    #             Pnm = ALFsGravityAnomaly(
-    #                 vartheta=theta, 
-    #                 nmax=self.nmax, 
-    #                 ellipsoid=self.ellipsoid,
-    #                 show_progress=leg_progress
-    #             )  # (chunk_size, nmax+1, nmax+1)
-                
-    #             H_flat[start:end] = compute_harmonic_sum(Pnm, self.HCnm, self.HSnm, cosm, sinm)
-        
-    #     return H_flat.reshape(input_shape)
     
     def dtm2006_height(
         self,
@@ -406,7 +219,7 @@ class DigitalTerrainModel:
             ]
             
             with Pool(processes=n_workers) as pool:
-                results = list(tqdm(pool.imap(process_chunk, chunks), total=len(chunks), desc='Computing chunks'))
+                results = list(tqdm(pool.imap(DigitalTerrainModel.process_chunk, chunks), total=len(chunks), desc='Computing chunks'))
             
             for start, result in zip(chunk_starts, results):
                 end = min(start + chunk_size, num_points)
