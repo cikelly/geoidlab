c14fd7c4e007a14f39e59b4c588c63326d2ac184
Caleb Kelly <calebaid2000@gmail.com>
2025-04-03 00:52:29 +0000

    Refactor plot.py to update import statement for gravity module

--- src/easy_pygeoid/plot.py
+++ src/easy_pygeoid/plot.py
@@ -6,7 +6,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import gravity as pygrav
+from . import gravity as pygrav
 import scipy.interpolate
 
 from mpl_toolkits.basemap import Basemap
