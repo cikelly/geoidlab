5fa0f9bfb5edd639ba1f2adc5c2fbd4e2b861cf8
Caleb Kelly <calebaid2000@gmail.com>
2025-04-26 21:18:55 +0000

    Refactor __init__.py and setup.py to include package description and license information

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+about = {}
+here  = Path(__file__).parent
+with open(here / 'geoidlab' / '__version__.py', 'r') as f:
+    exec(f.read(), about)
+
+with open(here / 'LICENSE', 'r') as f:
+    lincense_text = f.read()
+
+
 
 setup(
     name='geoidlab',
-    version='1.0',
+    version=about['__version__'],
     packages=find_packages(),
+    license=lincense_text,
     entry_points={
         'console_scripts': [
             'geoidlab=cli.geoidlab:main',
