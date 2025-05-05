2f26f3fd1550620d97c20fc2297f3ca47621ce30
Caleb Kelly <calebaid2000@gmail.com>
2025-05-05 19:22:16 +0000

    fix import

--- geoidlab/terrain.py
+++ geoidlab/terrain.py
@@ -14,7 +14,7 @@ import threading
 
 from geoidlab import constants
 from geoidlab.coordinates import geodetic2cartesian
-from geoidlab.utils.numba.terrain import (
+from geoidlab.numba.terrain import (
     compute_tc_chunk, 
     compute_rtm_tc_chunk, 
     compute_ind_chunk, 
